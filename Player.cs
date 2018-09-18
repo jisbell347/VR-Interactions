687d52f540ccb120fce7d715d038812444817273
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-09-18 11:58:22 -0600

    Added method to move player into castle when door is lowered.

--- Player.cs
+++ Player.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public float speed = 0.5f;
+
+    public Vector3 castlePosition;
+    // target position of the player
+    private Vector3 targetPosition;
 
 	// Use this for initialization
 	void Start () {
-		
+	    targetPosition = transform.parent.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    RaycastHit hit;
-
+        // Sends out a raycast in the forward direction and if it hits the Door Button component, calls the OnLook method from the Door Button Class.
 	    if (Physics.Raycast(transform.position, transform.forward, out hit)) {
 	        if (hit.transform.GetComponent<DoorButton>() != null) {
                 hit.transform.GetComponent<DoorButton>().OnLook();
+                MoveToCastle();
 	        }
 	    }
-	}
+
+	    transform.parent.position = Vector3.Lerp(transform.parent.position, targetPosition, Time.deltaTime * speed);
+     }
+
+    private void MoveToCastle() {
+        targetPosition = castlePosition;
+    }
 }
