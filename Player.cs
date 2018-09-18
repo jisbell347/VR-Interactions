6e9a44f1e99eccc61e67f84c5f170086a15b5f96
Joseph Isbell <39445858+jisbell347@users.noreply.github.com>
2018-09-18 13:00:33 -0600

    Added code to reset the game once all enemies are destroyed.

--- Player.cs
+++ Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour {
     public float speed = 0.5f;
 
     public Vector3 castlePosition;
+
+    public bool enteredCastle;
     // target position of the player
     private Vector3 targetPosition;
 
@@ -16,6 +18,7 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        // Look at the door
 	    RaycastHit hit;
         // Sends out a raycast in the forward direction and if it hits the Door Button component, calls the OnLook method from the Door Button Class.
 	    if (Physics.Raycast(transform.position, transform.forward, out hit)) {
@@ -25,10 +28,22 @@ public class Player : MonoBehaviour {
 	        }
 	    }
 
+        //Shooting at enemies
+	    if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("space")) {
+	        RaycastHit enemyHit;
+	        if (Physics.Raycast(transform.position, transform.forward, out enemyHit)) {
+	            if (hit.transform.GetComponent<Enemy>() != null) {
+                    Destroy(hit.transform.gameObject);
+	            }
+	        }
+	    }
+
+        //Moving Logic
 	    transform.parent.position = Vector3.Lerp(transform.parent.position, targetPosition, Time.deltaTime * speed);
      }
 
     private void MoveToCastle() {
+        enteredCastle = true;
         targetPosition = castlePosition;
     }
 }
